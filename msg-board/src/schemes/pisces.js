3652256328d041e2f707dc50d1c4bfdb17527e46
RainEffect <i@mc-rain.cn>
2019-04-28 12:12:44 +0800

    留言板测试

--- msg-board/src/schemes/pisces.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  var sidebarTop = $('.header-inner').height() + 10;
-
-  $('#sidebar').css({ 'margin-top': sidebarTop }).show();
-});
-<link rel="stylesheet" href="/css/spoiler.css" type="text/css"><script src="/js/spoiler.js" type="text/javascript" async></script>
